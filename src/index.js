ce6f4164f1904bbfc9ccf6ad6794c3b3212de538
Jacob Domingo <jc298378@dal.ca>
2024-05-26 19:55:31 -0300

    feat: create a base template for my website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css"; // Added styling for better UI
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Root component to manage the state and routing
+const Root = () => {
+  // useState hook to track the number of visits to the Home page
+  const [visits, setVisits] = useState(0);
+
+  // createBrowserRouter to define routes
+  const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <Error />,
+      },
+    {
+      path: "/home",
+      element: <Home visits={visits} setVisits={setVisits} />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/about",
+      element: <About visits={visits} />,
+      errorElement: <Error />,
+    },
+    {
+      path: "/contact",
+      element: <Contact visits={visits} />,
+      errorElement: <Error />,
+    },
+  ]);
+
+  // RouterProvider to provide the router to the application
+  return <RouterProvider router={router} />;
+};
+
+// Render the App component to the DOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
