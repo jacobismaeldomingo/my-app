271e691c0dd641de25b1cdfbcde2be0f6478f4be
Jacob Domingo <jc298378@dal.ca>
2024-05-26 19:56:01 -0300

    feat: Added the App component as the Welcome Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function App() {
+// App component serving as the Welcome Page with navigation links to Home, About, and Contact pages
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column gap-2 ms-3 mt-3">
+      <h1>Welcome Page</h1>
+      <h4>Simple State Management Website</h4>
+      <h5>CSCI 3130 - Jacob Domingo</h5>
+      <Link to="/home">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </div>
   );
-}
+};
 
 export default App;
